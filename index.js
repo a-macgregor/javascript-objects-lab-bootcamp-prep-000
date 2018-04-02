348aafc3140689bce4ca91b6298e386cdc58f5a1
A MacGregor <annabel.macgregor@gmail.com>
2018-04-02 08:26:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = new Object({});
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign(obj, { [key]: value });
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newRecipes = Object.assign({}, obj);
+  delete newRecipes.key;
+  return newRecipes;
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete recipes[key];
+  return recipes;
+}
